6a3975af9c979c859ed260975d0c22e7b59c73f3
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2022-08-31 06:45:20 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

---
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> lLevels = new ArrayList<>();
+        
+
+        
+        int height = maxDepth (root);
+        
+        for (int i = 1; i<=height; i++){
+            List<Integer> lElements = new ArrayList<>();
+            addLevelToList (root, i, lElements);
+            lLevels.add(i-1, lElements);
+            
+        }
+        
+        return lLevels;
+    }
+    
+    public void addLevelToList(TreeNode root, int level, List<Integer> lElements) {
+       
+        
+        if (root == null)
+            return;
+        else if (level == 1){
+            lElements.add(root.val);
+        }
+        else if (level > 1){
+            addLevelToList(root.left, level-1, lElements);
+            addLevelToList(root.right, level-1, lElements);
+        }
+        
+    }
+    
+    public static int maxDepth(TreeNode root) {
+        
+        if (root == null)
+            return 0;
+
+        else {
+            int lheight = maxDepth(root.left);
+            int rheight = maxDepth(root.right);
+            
+            if (lheight > rheight)
+                return (lheight + 1);
+            else 
+                return rheight+1;
+        }
+    }
+}

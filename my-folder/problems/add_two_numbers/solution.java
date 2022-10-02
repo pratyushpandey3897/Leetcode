a0ce64a49ed0b04e3470cb36b1f0b981137ef071
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2022-10-02 01:50:08 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- my-folder/problems/add_two_numbers/solution.java
+++ my-folder/problems/add_two_numbers/solution.java
@@ -11,23 +11,39 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
-    }
-        if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
-       return dummyHead.next;
+        ListNode addedList = new ListNode();
+        ListNode temp = addedList;
+        int c =0;
+        while(l1 != null || l2 != null || c!=0)
+        {
+            int sum = (l1==null? 0: l1.val) + (l2==null?0:l2.val) + c;
+            int digit = sum % 10;  // 9+7 = 16 >> 6 or 5+3 = 8 >> 8
+            c= sum/10;
+            addedList.next = new ListNode(digit);
+            
+            l1 = l1==null? l1:l1.next;
+            l2= l2==null? l2:l2.next;
+            addedList=addedList.next;
+            
+        }
+        
+        
+        
+        return temp.next;
+        
+        
     }
     
+//     public ListNode addition(ListNode l1, ListNode l2, int c, ListNode addedList){
+        
+//         if (l1 == null && l2 == null)
+//             return addedList;
+//         int sum = (l1==null? 0: l1.val) + (l2==null?0:l2.val) + c;
+//         int digit = sum % 10;  // 9+7 = 16 >> 6 or 5+3 = 8 >> 8
+//         c= sum/10;
+//         addedList.next = new ListNode(digit);
+        
+//         return addition(l1.next, l2.next, c, addedList.next);
+        
+//     }
 }

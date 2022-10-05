bf3d1fc4371eba186d2755ef24976ab44d2707c8
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2022-10-05 02:42:20 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- my-folder/problems/binary_tree_level_order_traversal/solution.java
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -14,52 +14,83 @@
  * }
  */
 class Solution {
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-        List<List<Integer>> lLevels = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         
 
         
-        int height = maxDepth (root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         
-        for (int i = 1; i<=height; i++){
-            List<Integer> lElements = new ArrayList<>();
-            addLevelToList (root, i, lElements);
-            lLevels.add(i-1, lElements);
+        while (!q.isEmpty()){
+            
+            List<Integer> levels = new ArrayList<>();
+            int levLen = q.size();
+            for (int i= 0; i<levLen; i++){
+                TreeNode node = q.remove();
+                if (node!=null){
+                    levels.add(node.val);
+                    q.add(node.left);
+                    q.add(node.right);
+                }
+            }
+            if (levels.size() != 0)
+                res.add(levels);
             
         }
         
-        return lLevels;
+        return res;
     }
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+//         List<List<Integer>> lLevels = new ArrayList<>();
+        
+
+        
+//         int height = maxDepth (root);
+        
+//         for (int i = 1; i<=height; i++){
+//             List<Integer> lElements = new ArrayList<>();
+//             addLevelToList (root, i, lElements);
+//             lLevels.add(i, lElements);
+            
+//         }
+        
+//         return lLevels;
+//     }
     
-    public void addLevelToList(TreeNode root, int level, List<Integer> lElements) {
+//     public void addLevelToList(TreeNode root, int level, List<Integer> lElements) {
        
         
-        if (root == null)
-            return;
-        else if (level == 1){
-            lElements.add(root.val);
-        }
-        else if (level > 1){
-            addLevelToList(root.left, level-1, lElements);
-            addLevelToList(root.right, level-1, lElements);
-        }
+//         if (root == null)
+//             return;
+//         else if (level == 1){
+//             lElements.add(root.val);
+//         }
+//         else if (level > 1){
+//             addLevelToList(root.left, level-1, lElements);
+//             addLevelToList(root.right, level-1, lElements);
+//         }
         
-    }
+//     }
     
-    public static int maxDepth(TreeNode root) {
+//     public static int maxDepth(TreeNode root) {
         
-        if (root == null)
-            return 0;
+//         if (root == null)
+//             return 0;
 
-        else {
-            int lheight = maxDepth(root.left);
-            int rheight = maxDepth(root.right);
+//         else {
+//             int lheight = maxDepth(root.left);
+//             int rheight = maxDepth(root.right);
             
-            if (lheight > rheight)
-                return (lheight + 1);
-            else 
-                return rheight+1;
-        }
-    }
+//             if (lheight > rheight)
+//                 return (lheight + 1);
+//             else 
+//                 return rheight+1;
+//         }
+//     }
+    
+    
 }

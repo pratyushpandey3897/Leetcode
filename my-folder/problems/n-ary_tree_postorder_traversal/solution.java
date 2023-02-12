48e521fdbfe36f8424bb70f53c5df891a25ef942
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-02-12 08:41:31 +0000

    Sync LeetCode submission - N-ary Tree Postorder Traversal (java)

---
+++ my-folder/problems/n-ary_tree_postorder_traversal/solution.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+        
+        dfs(root);
+        return ans;
+
+    }
+
+    public void dfs (Node node){
+
+        if (node == null)
+            return;
+        
+        for (Node child: node.children){
+            dfs(child);
+        }
+
+        
+        ans.add(node.val);
+
+    }
+}

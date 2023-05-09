cadbe45968f8b94eec96b0547ababfdcf2bb3cf2
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-05-09 06:34:33 +0000

    Sync LeetCode submission - All Elements in Two Binary Search Trees (java)

---
+++ my-folder/problems/all_elements_in_two_binary_search_trees/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        
+        List<Integer> res = new ArrayList<>();
+
+        
+        helper(root1, res);
+        helper(root2, res);
+        Collections.sort(res);
+        return res;
+        
+    }
+
+    public void helper(TreeNode root, List<Integer> res ){
+
+        if (root == null)
+            return;
+           
+        helper(root.left, res);
+        res.add(root.val);
+        helper(root.right, res);
+    }
+
+    
+}

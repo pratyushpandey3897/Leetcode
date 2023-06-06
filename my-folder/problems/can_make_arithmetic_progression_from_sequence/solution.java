c56b8832e6b94db1034f0d9bda7ccd814e74d633
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-06-06 04:18:51 +0000

    Sync LeetCode submission - Can Make Arithmetic Progression From Sequence (java)

---
+++ my-folder/problems/can_make_arithmetic_progression_from_sequence/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+
+        
+        Arrays.sort(arr);
+        // boolean ap = true;
+        int diff = arr[1] - arr[0];
+        for (int i = 2 ; i< arr.length; i++){
+            if (arr[i] == arr[i-1] + diff)
+                continue;
+            else{
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}

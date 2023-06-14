f855b75b2df8f81a364d5a4b3935a7a65ce696e9
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-06-14 22:32:18 +0000

    Sync LeetCode submission - Minimum Absolute Difference in BST (java)

---
+++ my-folder/problems/minimum_absolute_difference_in_bst/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diff = Integer.MAX_VALUE;
+    TreeNode prevNode = null;
+    public int getMinimumDifference(TreeNode root) {
+        
+        helper(root);
+        return diff;
+        
+    }
+
+    public void helper (TreeNode node){
+
+        if (node == null)
+        return;
+
+        helper(node.left);
+
+        if (prevNode !=null){
+            diff= Math.min(diff, Math.abs(prevNode.val - node.val));
+        }
+        prevNode = node;
+
+        helper(node.right);
+    }
+        
+}

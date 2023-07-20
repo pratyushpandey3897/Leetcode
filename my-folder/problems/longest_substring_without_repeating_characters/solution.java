8e71ceae31157854046b7d12f73c93126dc59e7e
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-07-20 04:37:25 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.java
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.java
@@ -1,39 +1,68 @@
 class Solution {
+    // public int lengthOfLongestSubstring(String s) {
+    //     int pointer1 = 0;
+    //     int pointer2 = 1;
+        
+    //     if (s.length() == 1)
+    //         return 1;
+        
+        
+    //     int max = 0;
+        
+    //     Queue<Character> queue = new LinkedList<>();
+        
+    //     for (int i = 0; i< s.length() ;i++){
+            
+            
+            
+    //         if(!queue.contains(s.charAt(i))){
+    //             queue.offer(s.charAt(i));
+                
+    //             max = max > queue.size() ? max : queue.size();
+    //         } else {
+                
+    //             while(queue.peek() != s.charAt(i)){
+    //                 queue.poll();
+    //             }
+                
+    //             queue.poll();
+    //             queue.offer(s.charAt(i));
+                
+    //             max = max > queue.size() ? max : queue.size();
+                
+    //         }
+            
+    //     }
+        
+    //     return max;
+    // }
+
     public int lengthOfLongestSubstring(String s) {
-        int pointer1 = 0;
-        int pointer2 = 1;
-        
-        if (s.length() == 1)
-            return 1;
-        
-        
-        int max = 0;
-        
-        Queue<Character> queue = new LinkedList<>();
-        
-        for (int i = 0; i< s.length() ;i++){
-            
-            
-            
-            if(!queue.contains(s.charAt(i))){
-                queue.offer(s.charAt(i));
-                
-                max = max > queue.size() ? max : queue.size();
+
+        if (s.length() == 0)
+            return 0;
+        Set<Character> set = new HashSet<>();
+
+        int l = 0;
+        set.add(s.charAt(l));
+        int r = 1;
+        int max = 1 ;
+        while (r < s.length() && l < s.length() ) {
+
+            if (!set.contains(s.charAt(r))){
+                set.add(s.charAt(r));
+                r++;
             } else {
-                
-                while(queue.peek() != s.charAt(i)){
-                    queue.poll();
-                }
-                
-                queue.poll();
-                queue.offer(s.charAt(i));
-                
-                max = max > queue.size() ? max : queue.size();
-                
+                set.remove(s.charAt(l));
+                l++;
             }
-            
+
+
+            max = max > set.size() ? max : set.size();
+
         }
-        
-        return max;
+
+        return max;    
+
     }
 }

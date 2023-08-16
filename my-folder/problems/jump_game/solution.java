be27dd24111a5357c30b0f43f431e29a7196dc3c
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-08-16 21:17:02 +0000

    Sync LeetCode submission - Jump Game (java)

--- my-folder/problems/jump_game/solution.java
+++ my-folder/problems/jump_game/solution.java
@@ -2,16 +2,14 @@ class Solution {
     public boolean canJump(int[] nums) {
         
         
-        int jumpsLeft = nums[0];
-        for (int i=1; i< nums.length; i++){
-            
-            if (jumpsLeft==0 )
+        int dist = 0;
+
+        for (int i =0 ;i < nums.length; i++){
+            if (i > dist)
                 return false;
-            
-            // we find if we have maximum jumps to land here
-            jumpsLeft = Math.max(nums[i], jumpsLeft-1);
-            
+            dist = Math.max(dist, nums[i]+i);
         }
+        
         
         return true;
     }

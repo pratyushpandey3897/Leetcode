ddf7c132e5715a32e07d8d9cf6285a89232d6914
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-08-16 03:01:34 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (java)

--- my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
+++ my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
@@ -4,27 +4,36 @@ class Solution {
         Stack<Pair> stack = new Stack<>();
         char[] chars = s.toCharArray();
         for (int i=0; i< chars.length; i++){
-            
                 
-            if (!stack.isEmpty() && stack.peek().getKey().equals(chars[i])){
-                stack.add(new Pair(stack.peek().getKey(), (int)stack.peek().getValue() +1));
+            if (!stack.isEmpty() && stack.peek().getKey().equals(chars[i])){ // if repeated elements
+                // stack.add(new Pair(stack.peek().getKey(), (int)stack.peek().getValue() +1));
+                int currValue = (int)stack.peek().getValue();
+                stack.pop();
+                stack.add(new Pair(chars[i], currValue+1));
             } else 
                 stack.add(new Pair(chars[i], 1));
 
             if ((int)stack.peek().getValue()== k){
-                int p = k;
-                while (p>0){
-                    stack.pop();
-                    p--;
-                }
+                // int p = k;
+                // while (p>0){
+                //     stack.pop();
+                //     p--;
+                // }
+
+                stack.pop();
             }
-
-
         }
 
         String res ="";
         while(!stack.isEmpty()){
-            res = stack.pop().getKey() + res;
+            int freq = (int)stack.peek().getValue();
+            String appendKey = String.valueOf(stack.peek().getKey()); 
+
+            while (freq>0) {
+                res = appendKey + res;
+                freq--;
+            }
+            stack.pop();
         }
 
         return res;

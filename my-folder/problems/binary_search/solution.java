176edbb6acc9d71e94bb80cde7c3af505133b7ea
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-08-18 15:01:17 +0000

    Sync LeetCode submission - Binary Search (java)

--- my-folder/problems/binary_search/solution.java
+++ my-folder/problems/binary_search/solution.java
@@ -1,24 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        if (nums.length <= 10000) {
-            int start = 0;
-            int end = nums.length - 1;
-            
-            while (start <= end) {
-                int mid = (start + end) / 2;
+        int left = 0;
+        int right = nums.length-1;
 
-                   if (target ==  nums[mid]) {
-                        return mid;
-                    }
-                    else if (target > nums[mid]){
-                        start = mid + 1;
-                    } 
-                    else if (target < nums[mid]) {
-                        end = mid - 1;
-                    }            
-            }
+        while (left<=right){
+            int mid = (left+right)/2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                right = mid-1;
+            else 
+                left = mid+1;
         }
-        return -1;    
+        return -1;
     }
 }

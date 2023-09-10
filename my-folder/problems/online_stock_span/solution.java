6a0a2a7adc91efade5f6d8ab610a3a51d7fbe416
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-09-10 06:22:14 +0000

    Sync LeetCode submission - Online Stock Span (java)

---
+++ my-folder/problems/online_stock_span/solution.java
@@ -0,0 +1,26 @@
+class StockSpanner {
+    private Stack <int []> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int ans = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price){
+
+            ans += stack.pop()[1];
+
+        }
+
+        stack.push(new int[]{price, ans});
+
+        return stack.peek()[1];
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */

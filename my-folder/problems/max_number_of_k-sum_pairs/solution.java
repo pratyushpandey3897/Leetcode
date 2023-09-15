d44376793c3f6223dc61af82f535841c4935401f
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-09-15 21:14:16 +0000

    Sync LeetCode submission - Max Number of K-Sum Pairs (java)

---
+++ my-folder/problems/max_number_of_k-sum_pairs/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        if (nums.length <=1)
+            return 0;
+        Arrays.sort(nums);
+        int ans = 0;
+        int left = 0;
+        int right = nums.length -1;
+
+        while (left < right){
+            int sum = nums[left] + nums[right];
+            if (sum > k)
+                right--;
+            else if (sum < k)
+                left ++;
+            else {
+                ans++;
+                left++;
+                right--;
+            }
+        }
+        return ans;
+    }
+}

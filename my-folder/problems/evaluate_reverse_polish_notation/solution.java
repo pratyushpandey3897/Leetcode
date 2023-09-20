0862c21da3a6dfdb79247a86716cb7e1b010395f
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-09-20 01:04:59 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (java)

--- my-folder/problems/evaluate_reverse_polish_notation/solution.java
+++ my-folder/problems/evaluate_reverse_polish_notation/solution.java
@@ -1,25 +1,28 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
-        Stack<Integer> stack = new Stack<>();
-        for (String e: tokens){
 
-                if (e.equals("+"))
-                    stack.push(stack.pop() + stack.pop());
-                else if (e.equals("*"))
-                    stack.push(stack.pop()*stack.pop());
-                else if (e.equals("-"))
-                    stack.push(-stack.pop() + stack.pop());
-                else if (e.equals("/"))
-                    stack.push(  (int)((1D/stack.pop()) * stack.pop()));
-                
-                else 
-                    stack.push(Integer.valueOf(e));
+        Stack<Integer> st = new Stack<>();
 
+        for (String s : tokens ){
 
+            if (s.equals("+")){
+                st.add(st.pop() + st.pop());
+            } else if (s.equals("-")){
+                st.add(-st.pop() + st.pop());
+            } else if (s.equals("*")){
+                st.add(st.pop()*st.pop());
+            } else if (s.equals("/")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.add(b/a);
+            } 
+
+            else {
+                st.add(Integer.valueOf(s));
+            }
         }
 
-        return stack.pop();
-
+        return st.pop();
+        
     }
 }

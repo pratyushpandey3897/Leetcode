6ac355bfe9d695df4374d6480703584a90cd1987
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-12-29 13:38:08 +0000

    Sync LeetCode submission - Permutations (java)

--- my-folder/problems/permutations/solution.java
+++ my-folder/problems/permutations/solution.java
@@ -1,42 +1,30 @@
 class Solution {
-    public List<List<Integer>> ans = new ArrayList<>();
-
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
         
-        int [] visited = new int[nums.length];
-
-        //int [] perm = new int[nums.length];
-        helper(new ArrayList<Integer>(), visited, nums, 0);
-
-        return ans;
+        backtrack(new ArrayList<>(), nums);
         
+        return ans;
 
     }
 
+    public void backtrack(List<Integer> curr, int[] nums){
 
-    public void helper(List<Integer> perm, int [] visited, int[] nums, int idx ){
-
-        
-        if (idx == nums.length){
-            //List<Integer> p = Arrays.stream(perm).boxed().collect(Collectors.toList());
-            ans.add(new ArrayList<>(perm));
+        if (curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
             return;
         }
 
-        for (int i=0; i< nums.length; i++){ // all possible elements
-
-            if (visited[i] == 1)
-                continue;
-            
-            else {
-                visited[i] = 1;
-                perm.add(nums[i]);
-                helper(perm, visited, nums, idx+1);
-                visited[i] = 0;
-                perm.remove(perm.size()-1);
+        for (int num: nums){
+            if (!curr.contains(num)){
+                curr.add(num);
+                backtrack(curr, nums);
+                curr.remove(curr.size()-1);
             }
         }
-            
 
+        return;
     }
+
+
 }

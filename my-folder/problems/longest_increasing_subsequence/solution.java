a9ec2f8ad1fa23374302e61b6be33cdc125f5eb7
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2023-12-29 14:46:56 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -1,39 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        
-        Integer [] lis = new Integer[nums.length];
-        int [] prevIdx = new int[nums.length];  // to record the actual subsequence
-        Arrays.fill(prevIdx,-1);
-        Arrays.fill(lis,1);
+
+        int [] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
         for (int i = 0; i< nums.length; i++){
 
-            for (int j = i ; j >=0 ; j--){
-                if (nums[i] > nums[j]){
-                    if (lis[i] < lis[j] + 1){
-                        lis[i] = lis[j] + 1;
-                        prevIdx[i] = j;
-                    }
-                    
+            for (int j = i; j>=0; j--){
+                if (nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[j]+1, dp[i]);
                 }
             }
         }
+
+        Arrays.sort(dp );
+        return dp[dp.length -1];
         
-        int maxidx = 0;
-        for (int i = 0; i< nums.length; i++){
-            if (lis[i] > lis[maxidx])
-                maxidx = i;
-        }
-
-        
-
-        int p = maxidx;
-        while (p >=0){
-            System.out.print(nums[p] + " ");
-            p = prevIdx[p];
-        }
-
-
-
-        return Collections.max(Arrays.asList(lis));
     }
 }

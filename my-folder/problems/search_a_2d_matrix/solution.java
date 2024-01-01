d72c0e354988c95e6563d5ea82ac8271d00ac8e6
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-01-01 04:30:37 +0000

    Sync LeetCode submission - Search a 2D Matrix (java)

--- my-folder/problems/search_a_2d_matrix/solution.java
+++ my-folder/problems/search_a_2d_matrix/solution.java
@@ -1,47 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-       
-        
-        int lenOfRows = matrix.length;
-        int lenOfColumns = matrix[0].length;
-        
-        if (lenOfRows == 0)
-            return false;
-        
-        int low = 0, high = lenOfRows;
-        
-        while (high-low>0){
-            int mid = (high + low) / 2;
-            
-            if (target >= matrix[mid][0] && target <= matrix[mid][(lenOfColumns-1)]){
-                // we found the row where target could be
-                return BinarySearchRow(matrix, target, mid);
-            } else if (target > matrix[mid][lenOfColumns-1]){
-                low = mid + 1;
-            } else {
-                high = mid;
+
+      
+        int n = matrix[0].length;
+        for (int [] row : matrix){
+
+            if (target >= row[0] && target <= row[n-1] ){
+
+                int low = 0, high = n-1;
+                while (low<=high){
+                    int mid = low - (low-high)/2;
+                    if (target == row[mid])
+                        return true;
+                    else if (target > row[mid]){
+                        low = mid+1;
+                    } else {
+                        high = mid-1;
+                    }
+                }
+
+                return false; 
             }
+
         }
-        
+
         return false;
         
-    }
-    
-    public static boolean BinarySearchRow(int[][] matrix, int target, int row){
-        
-        int low = 0, high = matrix[0].length;
-        
-        while (high - low > 0){
-            int mid = (high + low) / 2;
-            if (target == matrix[row][mid])
-                return true;
-            else if (target > matrix[row][mid])
-                low = mid + 1;
-            else if (target < matrix[row][mid])
-                high = mid;
-        }
-        
-        return false;
     }
 }

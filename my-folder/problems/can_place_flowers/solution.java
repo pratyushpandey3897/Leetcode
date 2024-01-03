1531dff88ffc245add4dbed51058baae7c971380
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-01-03 19:22:09 +0000

    Sync LeetCode submission - Can Place Flowers (java)

--- my-folder/problems/can_place_flowers/solution.java
+++ my-folder/problems/can_place_flowers/solution.java
@@ -1,34 +1,24 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        
+        int count = 0;
         for (int i = 0; i< flowerbed.length; i++){
 
             if (flowerbed[i] == 0 ){
+                boolean leftemp = i==0 || flowerbed[i-1] == 0;
+                boolean rightemp = i==flowerbed.length-1|| flowerbed[i+1] == 0;
+           
 
-                if (i > 0){
-                    if (flowerbed[i-1] == 0){
-                        if (i == flowerbed.length - 1 || flowerbed[i+1] == 0){
-                            
-                                System.out.println ("positions :" + i);
-                                flowerbed[i] = 1;
-                                n -=1;
-                            
-                        } 
-                    }
-                } else {
-                    if (i == flowerbed.length - 1 || flowerbed[i+1] == 0){
-                        
-                            flowerbed[i] = 1;
-                            n-=1;
-                        
-                    }
+            if (leftemp && rightemp){
+                flowerbed[i] = 1;
+                count ++;
+                if (count >= n){
+                    return true;
                 }
             }
+            }
 
-            if (n<=0)
-                return true;
         }
 
-        return false;
+        return count >= n;
     }
 }

c39b626cddd92ef634428f7f51314404221c5d4d
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-01-04 19:28:37 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -4,17 +4,29 @@ class Solution {
         int [] dp = new int[nums.length];
         Arrays.fill(dp, 1);
 
-        for (int i = 0; i< nums.length; i++){
+        // for (int i = 0; i< nums.length; i++){
 
-            for (int j = i; j>=0; j--){
-                if (nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[j]+1, dp[i]);
+        //     for (int j = i; j>=0; j--){
+        //         if (nums[j] < nums[i]){
+        //             dp[i] = Math.max(dp[j]+1, dp[i]);
+        //         }
+        //     }
+        // }
+
+        // Arrays.sort(dp );
+        // return dp[dp.length -1];
+
+        // dp[nums.length-1] = 1;
+        for (int i = nums.length - 2; i>=0; i--){
+
+            for (int j = i+1; j<nums.length; j++){
+                if (nums[i] < nums[j]){
+                    dp[i] = Math.max(dp[i], 1+dp[j] );
                 }
             }
         }
-
-        Arrays.sort(dp );
-        return dp[dp.length -1];
+        Arrays.sort(dp);
+        return dp[dp.length-1];
         
     }
 }

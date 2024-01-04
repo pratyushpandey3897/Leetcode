3bc0efd35b259c9cde323e11107e764af4c434d5
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-01-04 08:49:15 +0000

    Sync LeetCode submission - Non-overlapping Intervals (java)

--- my-folder/problems/non-overlapping_intervals/solution.java
+++ my-folder/problems/non-overlapping_intervals/solution.java
@@ -1,21 +1,19 @@
-import java.lang.Integer;
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals == null)
-            return 0;
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1],b[1]) );
-        int cnt = 1;
-        int prevFinishTime = intervals[0][1];
-        for (int i = 1; i< intervals.length; i++){
-            int currStartTime = intervals[i][0];
-            if (prevFinishTime <= currStartTime ){
-                // not overlapping
-                cnt++;
-                // assigning  the most recent finish time as the
-                //finish time of recently selected job
-                prevFinishTime = intervals[i][1];
+        int count = 0;
+        
+        Arrays.sort(intervals, (a,b) -> a[1] - b[1]);
+        int k = intervals[0][1];   
+        for (int i =1; i< intervals.length; i++){
+            
+            if (intervals[i][0] >= k )
+            {
+                k = intervals[i][1];
+            } else {
+                count++;
             }
         }
-        return intervals.length - cnt;
+
+        return count;
     }
 }

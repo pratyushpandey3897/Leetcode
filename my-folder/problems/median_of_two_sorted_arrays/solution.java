e28ce41aa67ef88351eb532cec0da7f02c3642a9
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-01-07 06:53:07 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

--- my-folder/problems/median_of_two_sorted_arrays/solution.java
+++ my-folder/problems/median_of_two_sorted_arrays/solution.java
@@ -1,31 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        // merging them
-        int n1 =0, n2=0, n3=0;
-        int[] arrMerged = new int[nums1.length+nums2.length];
+        int n = nums1.length + nums2.length;
+
+        int p1 = 0, p2 = 0;
         
-        while (n1 < nums1.length && n2 < nums2.length){
-            
-            if (nums1[n1] < nums2[n2])
-                arrMerged[n3++] = nums1[n1++];
-            else
-                arrMerged[n3++] = nums2[n2++];
-        }
-        
-        while (n1 < nums1.length)
-            arrMerged[n3++] = nums1[n1++];        
-        while (n2 < nums2.length)
-            arrMerged[n3++] = nums2[n2++]; 
-        double median;
-        
-        if (arrMerged.length % 2 != 0)
-            median = arrMerged[arrMerged.length/2];
-        else {
-            int temp =arrMerged.length/2;
-            median = (arrMerged[temp-1] + arrMerged[temp])/2.0;
+
+        int ele1 = 0;
+        int ele2 = 0;
+        for (int i = 0; i <= n/2; i++){
+            if (p1<nums1.length && p2<nums2.length){
+                if (nums1[p1] > nums2[p2]){
+                    ele1 = ele2;
+                    ele2 = nums2[p2];
+                    p2++;
+                } else {
+                    ele1 = ele2;
+                    ele2 = nums1[p1];
+                    p1++;
+                }
+            } else if (p1<nums1.length) {
+                ele1 = ele2; 
+                ele2 = nums1[p1];
+                p1++;
+            } else if (p2<nums2.length){
+                ele1 = ele2;
+                ele2 = nums2[p2];
+                p2++;
             }
+                
+        }
+
+        if (n%2 == 0)
+            return ((double)ele1+ ele2)/2;
         
-        return median;
+        return ele2;
     }
 }

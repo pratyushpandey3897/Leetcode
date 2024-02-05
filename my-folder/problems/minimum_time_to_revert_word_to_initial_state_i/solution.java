ed7fd023980e1b224cee765ca297be4a58327737
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-02-05 00:32:39 +0000

    Sync LeetCode submission - Minimum Time to Revert Word to Initial State I (java)

---
+++ my-folder/problems/minimum_time_to_revert_word_to_initial_state_i/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumTimeToInitialState(String word, int k) {
+        int i = 0;
+        int count = 0;
+        while (i < word.length() -k) {
+            String remaining = word.substring(i+k);
+            count++;
+
+            if (word.startsWith(remaining)){
+                return count;
+            } else {
+                i = i+k;
+            }
+        }
+
+        return count+1;
+    }
+}
+
+//abcbabcd
+// cbabcd** 1
+// abcd**** 2
+// cd****** 3

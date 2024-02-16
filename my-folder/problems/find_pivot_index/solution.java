1405907148e23c71543b2c3d072ad82ca63d1072
Pratyush Pandey <68020102+pratyushpandey3897@users.noreply.github.com>
2024-02-16 07:45:30 +0000

    Sync LeetCode submission - Find Pivot Index (java)

--- my-folder/problems/find_pivot_index/solution.java
+++ my-folder/problems/find_pivot_index/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int lsum[] = new int [nums.length];
-        int rsum[] = new int [nums.length];
-
-        for (int i=1, j = nums.length-2; i< nums.length && j>=0; i++,j--){
-            lsum[i] = lsum[i-1] + nums[i-1];
-            rsum[j] = rsum[j+1] + nums[j+1];
-
-        }
-
+        int totalsum = 0;
+        for (int a: nums)
+            totalsum +=a;
+        int psum = 0;
         for (int i = 0;i < nums.length; i++){
-            if (lsum[i] == rsum[i])
+            if (psum == totalsum - nums[i] -psum)
                 return i;
+            psum += nums[i];
         }
 
         return -1;
